de01ee3a3f83b564fd1c8cfce721f0d6e66162d6
babushkin-av <services@babushkin.ru.net>
2019-02-18 20:23:33 +0300

    developing!

--- current/WinDefWrapper.h
+++ current/WinDefWrapper.h
@@ -52,13 +52,29 @@
 #   define   CONST      const                                                                       // A variable whose value is to remain constant during execution.
 
     typedef  int        BOOL;                                                                       // A Boolean variable (should be TRUE or FALSE).
-    typedef  uint8_t    BYTE;                                                                       // A byte (8 bits).
-    typedef  uint16_t   WORD;                                                                       //
-    typedef  uint32_t   DWORD;                                                                      // A 32-bit unsigned integer.
-    typedef  uintptr_t  DWORD_PTR;                                                                  // An unsigned long type for pointer precision.
 
-    typedef  char       CHAR;                                                                       // An 8-bit Windows (ANSI) character.
+    typedef  char           CHAR;                                                                   // An 8-bit Windows (ANSI) character.
+    typedef  int            INT;                                                                    // A signed integer.
+    typedef  long           LONG;                                                                   // A signed long integer.
+    typedef  unsigned int   UINT;
+    typedef  unsigned long  ULONG;
 
+    typedef  int8_t         INT8                                                                    // An 8-bit signed integer.
+    typedef  int16_t        INT16                                                                   // A 16-bit signed integer.
+    typedef  int32_t        INT32                                                                   // A 32-bit signed integer.
+    typedef  int64_t        INT64                                                                   // A 64-bit signed integer.
+    typedef  uint8_t        UINT8                                                                   // An 8-bit unsigned integer.
+    typedef  uint16_t       UINT16                                                                  // A 16-bit unsigned integer.
+    typedef  uint32_t       UINT32                                                                  // A 32-bit unsigned integer.
+    typedef  uint64_t       UINT64                                                                  // A 64-bit unsigned integer.
+
+    typedef  uint8_t        BYTE;                                                                   // A byte (8 bits).
+    typedef  uint16_t       WORD;                                                                   //
+    typedef  uint32_t       DWORD;                                                                  // A 32-bit unsigned integer.
+    typedef  uint32_t       DWORD32;                                                                // A 32-bit unsigned integer.
+    typedef  uint64_t       QWORD;                                                                  // A 64-bit unsigned integer.
+    typedef  uint64_t       DWORD64;                                                                // A 64-bit unsigned integer.
+    typedef  uintptr_t      DWORD_PTR;                                                              // An unsigned long type for pointer precision.
 
 #endif                                                                                              // Detecting Windows
 
